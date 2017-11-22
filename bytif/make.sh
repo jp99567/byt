185e293219bae0a9c478f79e4c3f015d15e0024f
Juraj Podoba <juraj.podoba@gmail.com>
2017-11-22 13:42:25 +0100

    removed unwanted generated skeletons

--- bytif/make.sh
+++ bytif/make.sh
@@ -6,3 +6,4 @@ mkdir -p gen-req
 mkdir -p gen-ind
 thrift -r -gen cpp -out gen-ind indication.thrift
 thrift -r -gen cpp -out gen-req request.thrift
+rm gen-req/*.skeleton.cpp gen-ind/*.skeleton.cpp
