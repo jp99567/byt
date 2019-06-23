699f28f613f734c0332b125e34314060927c542f
Juraj Podoba <juraj.podoba@gmail.com>
2019-06-23 16:07:29 +0200

    removed st2cpp

--- st2cpp/b.sh
+++
@@ -1,5 +0,0 @@
-
-export QT5_VERSION=`ls -1 $HOME/Qt/|grep -P '^5.12.\d+$'|head -n1`
-echo $QT5_VERSION
-cmake ../st2cpp/ -DCMAKE_BUILD_TYPE=Debug -DCMAKE_PREFIX_PATH=/home/j/Qt/$QT5_VERSION/gcc_64/
-make
