6c7728fd013e4a46940bc04d370a4bdfe2ed6cbe
Juraj Podoba <juraj.podoba@gmail.com>
2019-12-19 18:17:29 +0100

    added events

---
+++ bytd/Event.h
@@ -0,0 +1,129 @@
+#pragma once
+
+#include "Subscription.h"
+#include <vector>
+#include <mutex>
+#include <atomic>
+#include <algorithm>
+
+namespace util{
+
+class IObserverConnection
+{
+public:
+    virtual ~IObserverConnection() = 0;
+    virtual std::unique_ptr<IObserverConnection> makeScopedDestructor() = 0;
+};
+
+using IObserverConnectionHandler = std::unique_ptr<IObserverConnection>;
+
+inline IObserverConnection::~IObserverConnection() = default;
+
+template<typename... Args>
+struct IEvent
+{
+    virtual ~IEvent(){}
+    virtual IObserverConnectionHandler subscribe(std::unique_ptr<ISubscription<void, Args...>>) = 0;
+    virtual void unsubscribe(std::unique_ptr<ISubscription<void, Args...>>) = 0;
+};
+
+template<typename... Args>
+class Event : public IEvent<Args...>
+{
+public:
+    using ISubscr    = ISubscription<void, Args...>;
+    using ISubscrPtr = std::unique_ptr<ISubscr>;
+
+    struct ObserversContainer
+    {
+        std::mutex              lock;
+        std::vector<ISubscrPtr> observers;
+    };
+
+    struct HandlerImpl : public IObserverConnection
+    {
+        std::weak_ptr<ObserversContainer> observers;
+        ISubscr*                          thisObserver = nullptr;
+        bool releaseInDestructor = false;
+
+        explicit HandlerImpl(std::weak_ptr<ObserversContainer> observers, ISubscr* thisObserver, bool release)
+            :observers(observers), thisObserver(thisObserver), releaseInDestructor(release)
+        {}
+
+        IObserverConnectionHandler makeScopedDestructor()
+        { 
+            this->releaseInDestructor = false;
+            auto handler = std::make_unique<HandlerImpl>(*this);
+            handler->releaseInDestructor = true;
+            return handler;
+        }
+
+        virtual ~HandlerImpl()
+        {
+            if (releaseInDestructor)
+            {
+                auto o = observers.lock();
+
+                if(o)
+                {
+                    unsubscr(o, [this](const ISubscrPtr& uPtr) { return uPtr.get() == this->thisObserver; });
+                }
+            }
+        }
+    };
+
+    virtual ~Event() = default;
+    Event()
+    {
+        mObservers = std::make_shared<ObserversContainer>();
+    }
+
+    IObserverConnectionHandler subscribe (std::unique_ptr<ISubscription<void, Args...>> observer) override
+    {
+        auto handler = std::make_unique<HandlerImpl>(mObservers, observer.get(), false);
+        std::lock_guard<std::mutex> lock(mObservers->lock);
+        mObservers->observers.push_back(std::move(observer));
+        return handler;
+    }
+
+    void unsubscribe (std::unique_ptr<ISubscription<void, Args...>> observer) override
+    {
+        unsubscr(this->mObservers, [&observer](const ISubscrPtr& uPtr) { return uPtr->isSame(*observer); });
+    }
+
+    void notify(Args... args)
+    {
+        std::vector<ISubscrPtr> tmp;
+        {
+            std::lock_guard<std::mutex> lock(mObservers->lock);
+            for(auto& obs : mObservers->observers)
+                tmp.push_back(obs->clone());
+        }
+
+        for(auto& obs : tmp)
+            obs->raise(args...);
+    }
+
+    void operator()(Args... args)
+    {
+        this->notify(args...);
+    }
+
+private:
+
+    template<typename Predicate>
+    static void unsubscr(std::shared_ptr<ObserversContainer> observers, Predicate predicate)
+    {
+        std::lock_guard<std::mutex> lock(observers->lock);
+        observers->observers.erase(
+            std::remove_if(observers->observers.begin(),
+                           observers->observers.end(),
+                           predicate
+            ),
+            observers->observers.end());
+    }
+
+    std::shared_ptr<ObserversContainer> mObservers;
+};
+
+}
---
+++ bytd/Subscription.h
@@ -0,0 +1,172 @@
+#pragma once
+
+#include <functional>
+#include <memory>
+
+namespace util{
+
+template<typename TResult = void, typename... TArgs>
+class ISubscription
+{
+public:
+    virtual ~ISubscription() { }
+    
+    // notifies observers about event occurence
+    virtual TResult raise(TArgs...) = 0;
+    
+    // compares if delagates reference same target method & listener
+    virtual bool isSame(const ISubscription&) const = 0;
+    
+    // Returns a deep copy of itself
+    virtual std::unique_ptr<ISubscription> clone() const = 0;
+};
+
+template<class TResult = void, typename... TArgs>
+class FSubscription : public ISubscription<TResult, TArgs...>
+{
+public:
+    typedef TResult (*Target)(TArgs...);
+    
+    FSubscription(Target target) : mTarget(target) { }
+    
+    TResult raise(TArgs... args) { return (*mTarget)(args...); }
+    
+    bool isSame(const ISubscription<TResult, TArgs...>& del) const
+    {
+        const FSubscription* other = reinterpret_cast<const FSubscription*>(&del);
+        return other && mTarget == other->mTarget;
+    }
+    
+    std::unique_ptr<ISubscription<TResult, TArgs...>> clone() const
+    {
+        return std::make_unique<FSubscription<TResult, TArgs...>>(mTarget);
+    }
+private:
+    Target mTarget;
+};
+
+template<class TResult = void, typename... TArgs>
+class LSubscription : public ISubscription<TResult, TArgs...>
+{
+public:
+    using Target = std::function<TResult(TArgs...)>;
+    
+    LSubscription(const Target& target) : mTarget(target) { }
+    
+    TResult raise(TArgs... args) { return mTarget(args...); }
+    
+    bool isSame(const ISubscription<TResult, TArgs...>&) const
+    {
+        return false; // C++ lambdas are simply bad.
+    }
+    
+    std::unique_ptr<ISubscription<TResult, TArgs...>> clone() const
+    {
+        return std::make_unique<LSubscription<TResult, TArgs...>>(mTarget);
+    }
+private:
+    Target mTarget;
+};
+
+namespace maXYmo_priv
+{
+
+template <bool CallbackConstness, class TListener, class TResult, typename...TArgs> struct CallbackType
+{ };
+
+template < class TListener, class TResult, typename...TArgs> struct CallbackType<true, TListener, TResult, TArgs...>
+{
+    typedef TResult (TListener::*Target)(TArgs...) const;
+};
+
+template < class TListener, class TResult, typename...TArgs> struct CallbackType<false, TListener, TResult, TArgs...>
+{
+    typedef TResult (TListener::*Target)(TArgs...);
+};
+
+}
+
+template<class TListener, class TResult, bool CallbackConstness, typename... TArgs>
+class Subscription : public ISubscription<TResult, TArgs...>
+{
+public:
+    using Target = typename maXYmo_priv::CallbackType<CallbackConstness, TListener, TResult, TArgs...>::Target;
+    
+    Subscription(TListener *listener, Target target) 
+        : mListener(listener), mTarget(target) {}
+    
+    TResult raise(TArgs... args)
+    {
+        return (mListener->*mTarget)(args...);
+    }
+    
+    bool isSame(const ISubscription<TResult, TArgs...>& del) const
+    {
+        const Subscription* other = reinterpret_cast<const Subscription*>(&del);
+        return other && mListener == other->mListener && mTarget == other->mTarget;
+    }
+
+    std::unique_ptr<ISubscription<TResult, TArgs...>> clone() const
+    {
+        return std::make_unique<Subscription<TListener,TResult, CallbackConstness, TArgs...>>(mListener, mTarget);
+    }
+    
+private:
+    TListener* mListener;
+    Target mTarget;
+};
+
+template<class TListener, class TResult = void, typename... TArgs>
+std::unique_ptr<ISubscription<TResult, TArgs...>> subscr(TListener *listener,  TResult (TListener::*target)(TArgs...) const)
+{
+    return std::make_unique<Subscription<TListener, TResult, true, TArgs...>>(listener, target);
+}
+
+template<class TListener, class TResult = void, typename... TArgs>
+std::unique_ptr<ISubscription<TResult, TArgs...>> subscr(TListener *listener,  TResult (TListener::*target)(TArgs...))
+{
+    return std::make_unique<Subscription<TListener, TResult, false, TArgs...>>(listener, target);
+}
+
+template <class TResult = void, typename... TArgs>
+std::unique_ptr<ISubscription<TResult, TArgs...>> subscr(TResult (*target)(TArgs...))
+{
+    return std::make_unique<FSubscription<TResult, TArgs...>>(target);
+}
+
+template<typename Lambda>
+struct lambda_traits
+ : public lambda_traits<decltype(&Lambda::operator())>
+{};
+
+template <typename ClassType, typename ReturnType, typename... Args>
+struct lambda_traits<ReturnType(ClassType::*)(Args...) const>
+{
+    using stdFunc = typename LSubscription<ReturnType, Args...>::Target;
+};
+
+template<typename Lambda>
+typename lambda_traits<Lambda>::stdFunc
+lambda_to_subscr(const Lambda& lambda)
+{
+    return static_cast<typename lambda_traits<Lambda>::stdFunc>(lambda);
+}
+
+namespace maXYmo_priv
+{
+
+template <class TResult = void, typename... TArgs>
+std::unique_ptr<ISubscription<TResult, TArgs...>> lambda_subscr_impl(const std::function<TResult(TArgs...)>& lambda)
+{
+    return std::make_unique<LSubscription<TResult, TArgs...>>(lambda);
+}
+
+}
+
+template <class Lambda, typename std::enable_if<!std::is_pointer<Lambda>::value || (!std::is_function<typename std::remove_pointer<Lambda>::type>::value)>::type* dummy = nullptr>
+decltype(auto) subscr(const Lambda& lambda)
+{
+    return maXYmo_priv::lambda_subscr_impl(lambda_to_subscr(lambda));
+}
+
+}
