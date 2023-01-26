eac025711946d96d26e954d6df0618d3e15a1121
Juraj Podoba <Juraj.Podoba@kistler.com>
2023-01-26 06:14:41 +0100

    improved start scripts

--- script/deploy/bb/init.sh
+++ script/deploy/bb/init.sh
@@ -4,10 +4,16 @@ set -e
 
 config-pin -f config-pin.txt
 
-RPROC_STATE='/sys/class/remoteproc/remoteproc1/state'
-if [[ 'running' == $(<$RPROC_STATE) ]]; then
-	echo stop > $RPROC_STATE
+RPROC_CTRL_FILE='/sys/class/remoteproc/remoteproc1/state'
+while [[ ! -f $RPROC_CTRL_FILE ]]; do
+	echo waiting for $RPROC_CTRL_FILE
+	sleep 5
+done
+RPROC_CUR_STATE=$(<$RPROC_CTRL_FILE)
+if [[ 'offline' != $RPROC_CUR_STATE ]]; then
+	echo $RPROC_CTRL_FILE $RPROC_CUR_STATE
+	echo stop > $RPROC_CTRL_FILE
 fi
 
-echo start > $RPROC_STATE
+echo start > $RPROC_CTRL_FILE
 
