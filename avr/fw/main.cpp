b8cd8e213f3891c345907c83166ccd46826f4fee
Juraj Podoba <Juraj.Podoba@kistler.com>
2023-01-26 22:49:37 +0100

    implemented fw init stage1 req/rsp loop

--- avr/fw/main.cpp
+++ avr/fw/main.cpp
@@ -5,10 +5,12 @@
 #include "ow.h"
 
 ISR(TIMER0_OVF_vect){
-	PIND = _BV(PD0);
 }
 
-constexpr uint8_t MCUSR_EXTENSION_NOT_BOOTLOADER = _BV(7);
+constexpr uint8_t cFwStageInit1 = _BV(7);
+constexpr uint8_t cFwStageInit2 = _BV(6);
+constexpr uint8_t cFwStageRunning = _BV(6) | _BV(7);
+static uint8_t gFwStage = cFwStageInit1;
 
 namespace ow {
 unsigned timer(void)
@@ -41,7 +43,7 @@ constexpr uint8_t u8min(uint8_t a, uint8_t b){
 
 static uint8_t msg[8] __attribute__ ((section (".noinit")));
 static uint8_t msglen __attribute__ ((section (".noinit")));
-bool can_rx()
+bool can_rx_svc()
 {
 	for(uint8_t id=13; id<=14; ++id){
 		CANPAGE = ( id << MOBNB0 );
@@ -59,14 +61,14 @@ bool can_rx()
 	return false;
 }
 
-void can_tx(void) {
+void can_tx_svc(void) {
 	CANPAGE = ( 12 << MOBNB0 );
 	while ( CANEN1 & ( 1 << ENMOB12 ) ); // Wait for MOb 0 to be free
 	CANSTMOB = 0x00;    	// Clear mob status register
 	for(uint8_t i=0; i<msglen; ++i)
 		CANMSG = msg[i];
 
-	CANCDMOB = ( 1 << CONMOB0 ) | ( 1 << IDE ) | ( msglen << DLC0 ); 	// Enable transmission, data length=1 (CAN Standard rev 2.0B(29 bit identifiers))
+	CANCDMOB = ( 1 << CONMOB0 ) | ( 1 << IDE ) | ( msglen << DLC0 );
 	while ( ! ( CANSTMOB & ( 1 << TXOK ) ) );	// wait for TXOK flag set
 	CANCDMOB = 0x00;
 	CANSTMOB = 0x00;
@@ -75,41 +77,37 @@ void can_tx(void) {
 void init()
 {
 	CANTCON = 99;
-	TCCR0A = 0b101;
-	DDRD |= _BV(PD0);
-	PORTD |= _BV(PD0);
-	TIMSK0 = 1;
-	sei();
+}
+
+void svc() {
+	switch (msg[0]) {
+	case 's':
+		msglen = 8;
+		msg[1] = CANTEC;
+		msg[2] = CANREC;
+		msg[3] = MCUSR | gFwStage;
+		msg[4] = CANIDT1;
+		msg[5] = CANIDT2;
+		msg[6] = CANIDT3;
+		msg[7] = CANIDT4;
+		break;
+	default:
+		msglen = 0;
+		break;
+	}
+
+	if(msglen > 0){
+		can_tx_svc();
+	}
 }
 
 int main() {
 
 	init();
 
-	msglen = 1;
-	msg[0] = 0xCD;
-	can_tx();
-	while(true){
-		can_rx();
-		switch(msg[0])
-		{
-		case 's':
-			msglen = 8;
-			msg[1] = CANTEC;
-			msg[2] = CANREC;
-			msg[3] = MCUSR|MCUSR_EXTENSION_NOT_BOOTLOADER;
-			msg[4] = CANIDT1;
-			msg[5] = CANIDT2;
-			msg[6] = CANIDT3;
-			msg[7] = CANIDT4;
-			break;
-		default:
-			break;
-		}
-
-		if(CANGIT & _BV(OVRTIM)){
-			CANGIT |= _BV(OVRTIM);
-			can_tx();
+	while(gFwStage == cFwStageInit1){
+		if(can_rx_svc()){
+			svc();
 		}
 	}
 	return 0;
