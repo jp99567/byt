54ca897e0ce0609bc3b367284621e9decec6a7ed
Juraj Podoba <Juraj.Podoba@gmail.com>
2023-02-16 22:07:55 +0100

    enabled shared from this of can output control class

--- bytd/candata.h
+++ bytd/candata.h
@@ -96,12 +96,20 @@ class DigiInItem : public IInputItem
     void update(const Data& data, std::size_t len) override;
 };
 
-class OutputControl : public IOutputControl
+class OutputControl : public IOutputControl, std::enable_shared_from_this<OutputControl>
 {
     void update(std::size_t idx, IOutputItem& item) override;
     void writeReady();
 
+    std::shared_ptr<OutputControl> getptr() {
+        return shared_from_this();
+    }
+    [[nodiscard]] static std::shared_ptr<OutputControl> create(ICanBus& canbus) {
+        return std::shared_ptr<OutputControl>(new OutputControl(canbus));
+    }
 private:
+    OutputControl(ICanBus& canbus):canbus(canbus){}
+
     std::vector<OutputMsg> outputs;
     ICanBus& canbus;
 };
