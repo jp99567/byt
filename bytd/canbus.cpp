bbdec560dbcde3444d7cc8d0ae7d598c691ca7d5
Juraj Podoba <Juraj.Podoba@gmail.com>
2023-02-18 12:08:06 +0100

    implemented canbus send recv

--- bytd/canbus.cpp
+++ bytd/canbus.cpp
@@ -12,6 +12,8 @@
 #include <linux/can/raw.h>
 
 CanBus::CanBus()
+    :onRecv([](const can_frame&){})
+    ,wrReady([]{})
 {
     struct sockaddr_can addr;
     struct ifreq ifr;
@@ -45,22 +47,66 @@ CanBus::~CanBus()
     }
 }
 
-void CanBus::read(can_frame &frame) const
+void CanBus::setOnRecv(std::function<void (const can_frame &)> cbk)
+{
+    std::lock_guard<std::mutex> ulck(lock);
+    onRecv = cbk;
+}
+
+void CanBus::setWrReadyCbk(std::function<void ()> cbk)
+{
+    std::lock_guard<std::mutex> ulck(lock);
+    wrReady = cbk;
+}
+
+bool operator==(const can_frame &a, const can_frame &b)
+{
+    if(a.can_id == b.can_id && a.can_dlc == b.can_dlc){
+        if(0 == ::memcmp(a.data, b.data, a.can_dlc))
+                return true;
+    }
+    return false;
+}
+
+bool CanBus::read(can_frame &frame)
 {
     if(socket == -1)
-        return;
+        return false;
     auto len = ::read(socket, &frame, sizeof(struct can_frame));
     if( len < (int)sizeof(struct can_frame)) {
         LogSYSDIE("CAN read");
     }
+    bool isWrReady = false;
+    {
+        std::lock_guard<std::mutex> ulck(lock);
+        if(pending){
+            if(frame == frame_wr){
+                pending = false;
+                isWrReady = true;
+            }
+        }
+    }
+    if(isWrReady){
+        wrReady();
+        return false;
+    }
+    onRecv(frame);
+    return true;
 }
 
-void CanBus::write(const can_frame &frame) const
+bool CanBus::write(const can_frame &frame)
 {
     if(socket == -1)
-        return;
-    auto len = ::write(socket, &frame, sizeof(struct can_frame));
-    if( len < (int)sizeof(struct can_frame)) {
-        LogSYSDIE("CAN write");
+        return false;
+    std::lock_guard<std::mutex> ulck(lock);
+    if( not pending){
+        pending = true;
+        frame_wr = frame;
+        auto len = ::write(socket, &frame_wr, sizeof(struct can_frame));
+        if( len < (int)sizeof(struct can_frame)) {
+            LogSYSDIE("CAN write");
+        }
+        return true;
     }
+    return false;
 }
--- bytd/canbus.h
+++ bytd/canbus.h
@@ -1,15 +1,25 @@
 #pragma once
-
-struct can_frame;
+#include <linux/can.h>
+#include <mutex>
+#include <functional>
 
 class CanBus
 {
     int socket = -1;
+    can_frame frame_wr;
+    bool pending = false;
+    std::mutex lock;
+
 public:
     CanBus();
     ~CanBus();
+    bool send(const can_frame& frame);
+    std::function<void(const can_frame& frame)> onRecv;
+    std::function<void()> wrReady;
+    void setOnRecv(std::function<void(const can_frame& frame)> cbk);
+    void setWrReadyCbk(std::function<void()> cbk);
 private:
-    void read(can_frame& frame) const;
-    void write(const can_frame& frame) const;
+    bool read(can_frame& frame);
+    bool write(const can_frame& frame);
 };
 
