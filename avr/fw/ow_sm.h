324eb8a2ce0120377b31893ebcfa747ff49c6825
Juraj Podoba <Juraj.Podoba@kistler.com>
2023-02-19 16:34:08 +0100

    added ow crc8 for avr

--- avr/fw/ow_sm.h
+++ avr/fw/ow_sm.h
@@ -7,8 +7,9 @@ namespace ow {
 void init(void);
 void search(bool direction);
 void read_bits(uint8_t count);
-void write_bits(uint8_t count, bool strong_power_req);
+void write_bits(uint8_t count, bool strong_power_req=false);
 ResponseCode response();
+uint8_t calc_crc(uint8_t b, uint8_t prev_b);
 }
 
 //
