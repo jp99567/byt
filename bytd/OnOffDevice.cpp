e40011e3e783b0fa24978f6b46266e2b6ac628e2
Juraj Podoba <Juraj.Podoba@gmail.com>
2023-06-14 17:57:39 +0200

    added monostable device

--- bytd/OnOffDevice.cpp
+++ bytd/OnOffDevice.cpp
@@ -3,10 +3,8 @@
 #include "Log.h"
 
 OnOffDevice::OnOffDevice(std::unique_ptr<IDigiOut> out, std::string name, std::shared_ptr<MqttClient> mqtt)
-    :out(std::move(out))
-    ,name(name)
-    ,mqttPath(std::string(mqttPathPrefix).append(name))
-    ,mqtt(mqtt){}
+    :DeviceBase(std::move(out), name, mqtt)
+{}
 
 void OnOffDevice::toggle()
 {
@@ -30,3 +28,33 @@ bool OnOffDevice::get() const
 {
     return *out;
 }
+
+MonoStableDev::MonoStableDev(std::unique_ptr<IDigiOut> out, std::string name, std::shared_ptr<MqttClient> mqtt, boost::asio::io_service& io_context, float timeout_sec)
+    :DeviceBase(std::move(out), name, mqtt)
+    ,timer(io_context)
+    ,timeout(std::chrono::milliseconds((int)(timeout_sec*1e3)))
+{}
+
+void MonoStableDev::trigger()
+{
+    if((*out)){
+        LogDBG("MonoStableDev already triggered");
+        return;
+    }
+
+    timer.expires_at(std::chrono::steady_clock::now() + timeout);
+    timer.async_wait([this](boost::system::error_code){
+        (*out)(false);
+        mqtt->publish(mqttPath, 0);
+        LogDBG("MonoStableDev cleared");
+    });
+    (*out)(true);
+    LogDBG("MonoStableDev triggered");
+}
+
+DeviceBase::DeviceBase(std::unique_ptr<IDigiOut> out, std::string name, std::shared_ptr<MqttClient> mqtt)
+    :out(std::move(out))
+    ,name(name)
+    ,mqttPath(std::string(mqttPathPrefix).append(name))
+    ,mqtt(mqtt){}
+
--- bytd/OnOffDevice.h
+++ bytd/OnOffDevice.h
@@ -2,21 +2,38 @@
 
 #include <memory>
 #include "mqtt.h"
+#include <boost/asio/io_service.hpp>
+#include <boost/asio/steady_timer.hpp>
 
 class IDigiOut;
 
-class OnOffDevice
+class DeviceBase
 {
 public:
-    explicit OnOffDevice(std::unique_ptr<IDigiOut> out, std::string name, std::shared_ptr<MqttClient> mqtt);
-    void toggle();
-    void set(bool on, bool by_mqtt = false);
-    bool get() const;
     static constexpr auto mqttPathPrefix = "rb/ctrl/dev/";
-private:
+protected:
+    explicit DeviceBase(std::unique_ptr<IDigiOut> out, std::string name, std::shared_ptr<MqttClient> mqtt);
     std::unique_ptr<IDigiOut> out;
     const std::string name;
     const std::string mqttPath;
     std::shared_ptr<MqttClient> mqtt;
 };
 
+class OnOffDevice : public DeviceBase
+{
+public:
+    explicit OnOffDevice(std::unique_ptr<IDigiOut> out, std::string name, std::shared_ptr<MqttClient> mqtt);
+    void toggle();
+    void set(bool on, bool by_mqtt = false);
+    bool get() const;
+};
+
+class MonoStableDev : public DeviceBase
+{
+public:
+    explicit MonoStableDev(std::unique_ptr<IDigiOut> out, std::string name, std::shared_ptr<MqttClient> mqtt, boost::asio::io_service& io_context, float timeout_sec);
+    void trigger();
+private:
+    boost::asio::steady_timer timer;
+    const std::chrono::milliseconds timeout;
+};
