cef974871a95bf73fd49d811d24ea19656588a1b
Juraj Podoba <Juraj.Podoba@kistler.com>
2023-10-22 11:51:07 +0200

    fixed pru build env paths

--- pru/env.sh
+++ pru/env.sh
@@ -1,3 +1,3 @@
-TI_SDK=/mnt/nfs/data2/ti-processor-sdk-linux-am335x-evm-04.03.00.05
+TI_SDK=/home/j/w/misc-repos/ti-processor-sdk-linux-am335x-evm-04.03.00.05
 export PRU_CGT=$TI_SDK/linux-devkit/sysroots/x86_64-arago-linux/usr/share/ti/cgt-pru
 export PRU_TI_EXAMPLE=$TI_SDK/example-applications/pru-icss-5.1.0
--- script/deploy/bb/deploy-pru.sh
+++ script/deploy/bb/deploy-pru.sh
@@ -2,5 +2,5 @@
 
 set -ex
 echo stop > /sys/class/remoteproc/remoteproc1/state
-cp /mnt/ws/workspace/repo/pru/gen/pru.out /lib/firmware/am335x-pru0-fw
+cp /mnt/w/byt/pru/gen/pru.out /lib/firmware/am335x-pru0-fw
 echo start > /sys/class/remoteproc/remoteproc1/state
