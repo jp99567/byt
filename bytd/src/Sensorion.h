671c35e59773a7f949fb60a5e5c874ee2fe329fc
Juraj Podoba <Juraj.Podoba@gmail.com>
2024-02-28 19:02:13 +0100

    added conversions for sensorion

--- bytd/src/Sensorion.h
+++ bytd/src/Sensorion.h
@@ -5,11 +5,33 @@
 
 namespace Sensorion {
 
-using Conversion = float(uint16_t);
+using Conversion = float(double);
 
-constexpr float convToDo(uint16_t v_u16)
+constexpr float convSHT11_T(double v_adc)
 {
-    return (float)v_u16;
+    constexpr float d1 = -39.6, d2 = 0.01;
+    return d1 + d2 * v_adc;
+}
+
+constexpr float convSHT11_RH(double v_adc)
+{
+    constexpr float c1 = -2.0468, c2 = 0.0367, c3 = -1.5955E-6;
+    return c1 + c2 * v_adc + c3 * v_adc * v_adc;
+}
+
+constexpr float convSCD41_T(double temp_adc)
+{
+    return -45 + 175 * (temp_adc / ((1<<16) - 1));
+}
+
+constexpr float convSCD41_RH(double rh_adc)
+{
+    return 100 * (rh_adc / ((1<<16) - 1));
+}
+
+constexpr float convSCD41_CO2(double v_adc)
+{
+    return v_adc;
 }
 
 template <Conversion covFn>
