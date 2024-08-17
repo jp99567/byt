500b98bc8f558a3c36ed0fb65f7495979a540b8d
Juraj Podoba <Juraj.Podoba@gmail.com>
2024-08-17 14:56:16 +0200

    improved backup script

--- script/backup-emmc.sh
+++ script/backup-emmc.sh
@@ -6,12 +6,13 @@ if [[ -n "$1" ]]; then
 	destdir="$1"
 else
 	mntdir=/mnt/w
-	mountpoint $mntdir || mount x509fa:/home/j/w $mntdir
-	destdir=$mntdir/bbb-img-backup
+	mountpoint $mntdir || mount vivo:/home/j/w $mntdir
+	destdir=$mntdir/bb-img-backup
 fi
 
 [[ -d "$destdir" ]]
 
 indev=/dev/mmcblk1
-ofile="$(hostname)-$(date +"%F_%T").img"
+mac=$(ip link show eth0|grep -oP 'ether .+ brd'|grep -oP '..:..:..:..:..'|tr -d :)
+ofile="bb-mac${mac}-$(date +"%F_%T").img"
 dd if=$indev of=$destdir/$ofile bs=1M status=progress
