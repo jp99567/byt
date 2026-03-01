9dfd29418e6fec379e6900837871ba8f897eac6a
Juraj Podoba <Juraj.Podoba@gmail.com>
2026-03-01 16:48:53 +0100

    removed unused

--- bytd/src/OpenTherm.h
+++ bytd/src/OpenTherm.h
@@ -12,21 +12,10 @@
 #include <functional>
 #include <memory>
 #include <thread>
-#include <vector>
 
 class Pru;
 class PruRxMsg;
 class MqttClient;
-
-struct OtParam {
-    int id;
-    uint16_t val;
-    bool updated = false;
-};
-
-struct OtWrParam : OtParam {
-    uint16_t val_mirrored;
-};
 
 class OpenTherm {
 public:
@@ -60,8 +49,6 @@ private:
     bool shutdown = false;
     std::shared_ptr<Pru> pru;
     std::shared_ptr<PruRxMsg> rxMsg;
-    std::vector<OtParam> rdParams;
-    std::vector<OtWrParam> wrParams;
     IMqttPublisherSPtr mqtt;
     uint16_t status = 0;
     opentherm::Frame asyncReq;
